0506ab5d9dcd02ddaaa37a7e30fa192cb5ef3bf7
Marlon Alex Andrade <marlonalexandrade@gmail.com>
2024-04-11 11:57:54 -0300

    update

--- secao08/ex71.py
+++ secao08/ex71.py
@@ -1,21 +1,16 @@
-class Ponto:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 def dentro_ret(v1, v2, p):
-    if v1.x <= p.x <= v2.x and v1.y <= p.y <= v2.y:
+    if v1[0] <= p[0] <= v2[0] and v1[1] <= p[1] <= v2[1]:
         return True  
     else:
         return False  
 
-v1 = Ponto(1, 1)  
-v2 = Ponto(5, 5)  
-p = Ponto(3, 3)   
+v1 = (1, 1)
+v2 = (5, 5)
+p = (3, 3)
 
 resultado = dentro_ret(v1, v2, p)
 
 if resultado:
-    print(f"O ponto ({p.x}, {p.y}) está dentro do retângulo.")
+    print(f"O ponto ({p[0]}, {p[1]}) está dentro do retângulo.")
 else:
-    print(f"O ponto ({p.x}, {p.y}) está fora do retângulo.")
+    print(f"O ponto ({p[0]}, {p[1]}) está fora do retângulo.")
--- secao08/ex72.py
+++ secao08/ex72.py
@@ -1,18 +1,12 @@
-class Vetor:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
 def soma(v1, v2):
-    res_x = v1.x + v2.x
-    res_y = v1.y + v2.y
-    res_z = v1.z + v2.z
-    return Vetor(res_x, res_y, res_z)
+    res_x = v1[0] + v2[0]
+    res_y = v1[1] + v2[1]
+    res_z = v1[2] + v2[2]
+    return (res_x, res_y, res_z)
 
-v1 = Vetor(1.0, 2.0, 3.0)
-v2 = Vetor(4.0, 5.0, 6.0)
+v1 = (1.0, 2.0, 3.0)
+v2 = (4.0, 5.0, 6.0)
 
 resultado = soma(v1, v2)
 
-print(f"Resultado da soma: ({resultado.x}, {resultado.y}, {resultado.z})")
+print(f"Resultado da soma: {resultado}")
